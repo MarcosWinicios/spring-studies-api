f0c7e9a2242d3a0e7a824f5489c609d11c7bd048
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-02-06 00:21:17 -0300

    refactor: clean project

--- src/main/java/com/studies/algafood/di/notification/NotifierEmail.java
+++ src/main/java/com/studies/algafood/di/notification/NotifierEmail.java
@@ -1,18 +1,12 @@
 package com.studies.algafood.di.notification;
 
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import com.studies.algafood.di.model.Client;
 
-@Profile("prod")
 @NotifierType(LevelUrgency.NO_URGENCY)
 @Component
 public class NotifierEmail implements Notifier {
-
-	public NotifierEmail() {
-		System.out.println("NotificadorEmail REAL");
-	}
 	
 	@Override
 	public void notify(Client client, String message) {
