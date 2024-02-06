51182d1e27ec74946d027a9b6fb05f07a1fea75e
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-02-05 23:07:48 -0300

    feat: adding enviroment config

--- src/main/java/com/studies/algafood/di/notification/NotifierEmailMock.java
+++ src/main/java/com/studies/algafood/di/notification/NotifierEmailMock.java
@@ -5,19 +5,19 @@ import org.springframework.stereotype.Component;
 
 import com.studies.algafood.di.model.Client;
 
-@Profile("prod")
+@Profile("dev")
 @NotifierType(LevelUrgency.NO_URGENCY)
 @Component
-public class NotifierEmail implements Notifier {
-
-	public NotifierEmail() {
-		System.out.println("NotificadorEmail REAL");
-	}
+public class NotifierEmailMock implements Notifier {
 	
+	public NotifierEmailMock() {
+		System.out.println("NotificadorEmail MOCK");
+	}
+
 	@Override
 	public void notify(Client client, String message) {
 
-		System.out.printf("Notificando %s através do e-mail %s : %s\n", client.getName(), client.getEmail(), message);
+		System.out.printf("MOCK: Notificação seria enviada para %s através do e-mail %s : %s\n", client.getName(), client.getEmail(), message);
 	}
 
 }
