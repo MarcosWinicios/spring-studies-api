ed19d1a6a1dfb26ea49a831c2679b0b64d2038f7
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-02-06 00:36:19 -0300

    config: customizing enviroment variables

--- src/main/java/com/studies/algafood/di/notification/NotifierEmail.java
+++ src/main/java/com/studies/algafood/di/notification/NotifierEmail.java
@@ -1,5 +1,6 @@
 package com.studies.algafood.di.notification;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.studies.algafood.di.model.Client;
@@ -8,8 +9,17 @@ import com.studies.algafood.di.model.Client;
 @Component
 public class NotifierEmail implements Notifier {
 	
+	@Value("${notifier.email.host-server}")
+	private String host;
+	
+	@Value("${notifier.email.port-server}")
+	private Integer port;
+	
 	@Override
 	public void notify(Client client, String message) {
+		
+		System.out.println("Host: " + host);
+		System.out.println("Port: " + port);
 
 		System.out.printf("Notificando %s através do e-mail %s : %s\n", client.getName(), client.getEmail(), message);
 	}
